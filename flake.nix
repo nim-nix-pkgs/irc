e0e7d48456a7ea59e21f5d6841618356266eb039
hugosenari <hugosenari@gmail.com>
2022-03-12 02:02:25 -0300

    chore: re index irc

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = ''Implements a simple IRC client.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."irc-master".url = "path:./master";
+  inputs."irc-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."irc-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."irc-v0_2_0".url = "path:./v0_2_0";
+  inputs."irc-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."irc-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."irc-v0_2_1".url = "path:./v0_2_1";
+  inputs."irc-v0_2_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."irc-v0_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."irc-v0_3_0".url = "path:./v0_3_0";
+  inputs."irc-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."irc-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."irc-v0_4_0".url = "path:./v0_4_0";
+  inputs."irc-v0_4_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."irc-v0_4_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_3_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Implements a simple IRC client.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-irc-v0_3_0.flake = false;
+  inputs.src-irc-v0_3_0.owner = "nim-lang";
+  inputs.src-irc-v0_3_0.ref   = "refs/tags/v0.3.0";
+  inputs.src-irc-v0_3_0.repo  = "irc";
+  inputs.src-irc-v0_3_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-irc-v0_3_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-irc-v0_3_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
